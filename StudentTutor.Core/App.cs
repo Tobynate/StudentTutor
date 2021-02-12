65d70be32a9141d9980f7d0c00bcc1ade1af8cd1
Tobynate <nathanieltoby99@gmail.com>
2021-02-12 13:09:20 +0100

    Setting Up Student Tutor to run with MVVMCROSS

---
+++ StudentTutor.Core/App.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.ViewModels;
+using StudentTutor.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentTutor.Core
+{
+    public class App: MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<CreateTutorialViewModel>();
+        }
+    }
+}
---
+++ StudentTutor.Core/Models/TutorialModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentTutor.Core.Models
+{
+    public class TutorialModel
+    {
+    }
+}
